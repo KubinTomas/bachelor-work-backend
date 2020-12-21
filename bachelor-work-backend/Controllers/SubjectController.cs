a9c840d8f59035bef2bf465a3cb446ee45ffe1f9
TomasKubin <Kubin.Tom@email.cz>
2020-12-21 18:47:01 +0100

    add subject dto add connection string to local db

--- bachelor-work-backend/Controllers/SubjectController.cs
+++ bachelor-work-backend/Controllers/SubjectController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using bachelor_work_backend.DTO.subject;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,10 +14,24 @@ namespace bachelor_work_backend.Controllers
     [Authorize]
     public class SubjectController : ControllerBase
     {
+        public SubjectController()
+        {
+
+        }
+
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
         }
+
+        [HttpPost, Route("create")]
+        public async Task<IActionResult> Create(SubjectDTO subjectDTO)
+        {
+
+       
+            return Ok();
+        }
+
     }
 }
---
+++ bachelor-work-backend/DTO/subject/SubjectDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bachelor_work_backend.DTO.subject
+{
+    public class SubjectDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Katedra { get; set; }
+        public string? Description { get; set; }
+    }
+}
